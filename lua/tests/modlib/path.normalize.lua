40620622863e44c23424a38239a5e3c822236648
JoshPiper <32164094+JoshPiper@users.noreply.github.com>
2022-08-04 16:35:05 +0100

    test(path.Normalize): Add tests for path normalization.

---
+++ lua/tests/modlib/path.normalize.lua
@@ -0,0 +1,31 @@
+local path = ModLib.Path
+
+local suite = {
+	groupName = "ModLib.Path.Normalize",
+	cases = {}
+}
+
+local paths = {
+	{"", "."},
+	{".", "."},
+	{"./", "."},
+	{"./dir", "dir"},
+	{"dir/..", "."},
+	{"dir/../dir", "dir"},
+	{"/root", "/root"},
+	{"/root/", "/root"},
+	{"/root/dir", "/root/dir"},
+	{"/root/dir/", "/root/dir"},
+	{"/root/dir/../", "/root"},
+	{"//root//dir//..//", "/root"}
+}
+for _, spec in ipairs(paths) do
+	table.insert(suite.cases, {
+		name = string.format("Normalise %s to %s", spec[1], spec[2]),
+		func = function()
+			expect(path:Normalize(spec[1])).to.eq(spec[2])
+		end
+	})
+end
+
+return suite
