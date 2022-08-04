739423f272e2bf9c3711729f61b7d625712e5109
JoshPiper <32164094+JoshPiper@users.noreply.github.com>
2022-08-04 15:32:27 +0100

    test(path): We use a : not . for path.

--- lua/tests/modlib/path.join.lua
+++ lua/tests/modlib/path.join.lua
@@ -15,7 +15,7 @@ for _, spec in ipairs(paths) do
 	table.insert(suite.cases, {
 		name = "Correctly Join Paths (" .. spec[1] .. ", " .. spec[2] .. ") => " .. spec[3],
 		func = function()
-			expect(path.Join(spec[1], spec[2])).to.eq(spec[3])
+			expect(path:Join(spec[1], spec[2])).to.eq(spec[3])
 		end
 	})
 end
