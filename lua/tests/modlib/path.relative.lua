585904b56f7c8a99a892bb40082f6d2e6ca2b5e3
JoshPiper <32164094+JoshPiper@users.noreply.github.com>
2022-10-11 13:07:39 +0100

    test(path): Add relative path tests.

---
+++ lua/tests/modlib/path.relative.lua
@@ -0,0 +1,36 @@
+local path = ModLib.Path
+
+local suite = {
+	groupName = "ModLib.Path.Relative",
+	cases = {
+		{
+			name = "Relative Path must fail.",
+			func = function()
+				expect(path:Relative("root", "/")).to.errWith("Input path must be absolute.")
+			end
+		}, {
+			name = "Relative Root must fail.",
+			func = function()
+				expect(path:Relative("/root", "root")).to.errWith("Root path must be absolute.")
+			end
+		}
+	}
+}
+
+local paths = {
+	{"/root", "/root", "."},
+	{"/root/directory/with/files", "/root", "directory/with/files"},
+	{"/root/directory/with/files", "/root/directory", "with/files"},
+	{"/root/directory/with/files", "/root/directory/with", "files"},
+	{"/root/directory/with/files", "/root/directory/with/files", "."},
+}
+for _, spec in ipairs(paths) do
+	table.insert(suite.cases, {
+		name = string.format("Get %s as a relative path from %s.", spec[1], spec[2]),
+		func = function()
+			expect(path:Relative(spec[1], spec[2])).to.eq(spec[3])
+		end
+	})
+end
+
+return suite
